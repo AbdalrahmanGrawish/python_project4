f38e9584bf70a07b40b3d4d0ee7050c54e5a5ea4
AbdalrahmanGrawish <145158607+AbdalrahmanGrawish@users.noreply.github.com>
2023-09-17 01:07:00 +0300

    Add files via upload
    Grade assessment of a student
    The function of this project is to assess the grade of a student in an exam. If the student scores more than or
    equal 90, he will achieve "A" . If the student scores more than 80 and less than 90 he will get "B". If the student
    scores more than 70 and less than 80 he will get "C" . Finally, if he scores less than 50, he will get "F"

---
+++ student grades .py
@@ -0,0 +1,15 @@
+student = int(input('please enter your number'))
+
+if student >= 90:
+  print('A')
+
+elif student >= 80:
+  print('B')
+
+elif student >= 70:
+  print('C')
+
+elif student >= 60:
+  print('D')
+else:
+  print('F') 
